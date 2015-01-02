6711a5a156275ea3957e21a7ccf0784c5ed59132
mcapitan <miriamcapitan@gmail.com>
2015-01-02 20:30:20 +0100

    Añado escribir en el log

--- uaserver.py
+++ uaserver.py
@@ -7,16 +7,22 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import SocketServer
 import sys
 import os
+import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-config = sys.argv[2]
+config = sys.argv[1]
 
 
-#Clase para manejar el xml
 class ServerHandler(ContentHandler):
+    """
+    Clase para manejar xml
+    """
 
     def __init__(self):
+        """
+        Constructor. Inicializamos las variables
+        """
         self.username = ''
         self.passwd = ''
         self.ip_uaserver = ''
@@ -62,26 +68,43 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                     reply = 'SIP/2.0 100 Trying\r\n\r\n'
                     reply += 'SIP/2.0 180 Ringing\r\n\r\n'
                     reply += 'SIP/2.0 200 OK\r\n\r\n'
-                    reply += metodo + ' sip:' + sys.argv[3] + ' SIP/2.0\r\n'
                     reply += 'Content-Type: application/sdp\r\n\r\n'
                     reply += 'v=0\r\n' + 'o=' + cHandler.username + ' '
                     reply += cHandler.ip_uaserver + '\r\n' + 's=misesion\r\n'
                     reply += 't=0\r\n' + 'm=audio ' + cHandler.puerto_rtpaudio
                     reply +=' RTP'
                     self.wfile.write(reply)
-                    print reply
+                    #Envío el mensaje
+                    my_socket.send(reply + '\r\n')
+                    print 'Enviando: ' + reply
+                    #Escribo en el log
+                    fichero.write(str(time) + ' Received from '
+                    + cHandler.ip_regproxy + ":"
+                    + str(cHandler.puerto_regproxy) + ": " + line)
+                    fichero.write(str(time) + ' Send to '
+                    + cHandler.ip_regproxy + ":"
+                    + str(cHandler.puerto_regproxy) + ": " + reply)
                 elif line.split(' ')[0] == 'BYE':
                     reply = 'SIP/2.0 200 OK\r\n\r\n'
-                    reply += metodo + ' sip:' + sys.argv[3] + ' SIP/2.0\r\n'
                     reply += 'Content-Type: application/sdp\r\n\r\n'
                     reply += 'v=0\r\n' + 'o=' + cHandler.username + ' '
                     reply += cHandler.ip_uaserver + '\r\n' + 's=misesion\r\n'
                     reply += 't=0\r\n' + 'm=audio ' + cHandler.puerto_rtpaudio
                     reply +=' RTP'
                     self.wfile.write(reply)
-                    print reply
+                    #Envío el mensaje
+                    my_socket.send(reply + '\r\n')
+                    print 'Enviando: ' + reply
+                    #Escribo en el log
+                    fichero.write(str(time) + ' Received from '
+                    + cHandler.ip_regproxy + ":"
+                    + str(cHandler.puerto_regproxy) + ": " + line)
+                    fichero.write(str(time) + ' Send to '
+                    + cHandler.ip_regproxy + ":"
+                    + str(cHandler.puerto_regproxy) + ": " + reply)
                 elif line.split(' ')[0] == 'ACK':
-                    aEjecutar = './mp32rtp -i ' + IP + ' -p 23032 < '
+                    aEjecutar = './mp32rtp -i ' + cHandler.ip_uaserver + ' -p '
+                    aEjecutar += cHandler.puerto_uaserver + ' < '
                     aEjecutar += cHandler.audio
                     print "Vamos a ejecutar", aEjecutar
                     os.system(aEjecutar)
@@ -96,7 +119,18 @@ if __name__ == "__main__":
     cHandler = ServerHandler()
     parser.setContentHandler(cHandler)
     parser.parse(open(config))
+
+    #Fichero de log
+    time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+    fichero = open(cHandler.path_log, "a")
+
+if len(sys.argv) != 2 and sys.argv[1] != config:
+    print('Usage: python uaserver.py config')
+    print('SIP/2.0 400 Bad Request')
+    sys.exit()
+else:
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer((IP, puerto), EchoHandler)
+    serv = SocketServer.UDPServer((cHandler.ip_uaserver,
+    int(cHandler.puerto_uaserver)), EchoHandler)
     print "Listening...\r\n"
     serv.serve_forever()
