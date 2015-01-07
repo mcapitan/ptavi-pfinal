427f4f2b2cd11768b0e775db316793dc1473b06c
mcapitan <miriamcapitan@gmail.com>
2015-01-07 12:15:35 +0100

    Corrijo 'Usage..' para que funcione

--- uaclient.py
+++ uaclient.py
@@ -11,11 +11,6 @@ import time
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
-config = str(sys.argv[1])
-metodo = str(sys.argv[2])
-opcion = (sys.argv[3])
-
-
 
 class ClientHandler(ContentHandler):
     """
@@ -39,32 +34,36 @@ class ClientHandler(ContentHandler):
     def startElement(self, name, attrs):
 
         if name == 'account':
-            self.username = str(attrs.get('username', ""))
-            self.passwd = str(attrs.get('passwd', ""))
+            self.username = attrs.get('username', "")
+            self.passwd = attrs.get('passwd', "")
         elif name == 'uaserver':
-            self.ip_uaserver = str(attrs.get('ip', ""))
-            self.puerto_uaserver = int(attrs.get('puerto', ""))
+            self.ip_uaserver = attrs.get('ip', "")
+            self.puerto_uaserver = attrs.get('puerto', "")
         elif name == 'rtpaudio':
-            self.puerto_rtpaudio = int(attrs.get('puerto', ""))
+            self.puerto_rtpaudio = attrs.get('puerto', "")
         elif name == 'regproxy':
-            self.ip_regproxy = str(attrs.get('ip', ""))
-            self.puerto_regproxy = int(attrs.get('puerto', ""))
+            self.ip_regproxy = attrs.get('ip', "")
+            self.puerto_regproxy = attrs.get('puerto', "")
         elif name == 'log':
-            self.path_log = str(attrs.get('path', ""))
+            self.path_log = attrs.get('path', "")
         elif name == 'audio':
-            self.path_audio = str(attrs.get('path', ""))
+            self.path_audio = attrs.get('path', "")
 
 
 if __name__ == "__main__":
     parser = make_parser()
     cHandler = ClientHandler()
     parser.setContentHandler(cHandler)
+    config = str(sys.argv[1])
     parser.parse(open(config))
 
-if len(sys.argv) != 4:
-    print('Usage: python uaclient.py config method option')
-    sys.exit()
-else:
+    if len(sys.argv) != 4:
+        print('Usage: python uaclient.py config method option')
+        sys.exit()
+
+    metodo = str(sys.argv[2])
+    opcion = (sys.argv[3])
+
     # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -78,7 +77,7 @@ else:
     try:
         if metodo == 'REGISTER':
             LINE = metodo + ' sip:' + cHandler.username + ":"
-            LINE += cHandler.ip_uaserver + ' SIP/2.0\r\n'
+            LINE += cHandler.puerto_uaserver + ' SIP/2.0\r\n'
             LINE += 'Expires: ' + opcion + '\r\n'
             #Envío el mensaje
             my_socket.send(LINE + '\r\n')
@@ -116,6 +115,13 @@ else:
             LINE1 = 'ACK sip:' + opcion + ' SIP/2.0'
             my_socket.send(LINE1 + '\r\n')
             print 'Enviando: ' + LINE1
+            #Intercambio de RTP
+            aEjecutar = './mp32rtp -i ' + cHandler.ip_uaserver + ' -p '
+            aEjecutar += cHandler.puerto_uaserver + ' < '
+            aEjecutar += cHandler.audio
+            print "Vamos a ejecutar", aEjecutar
+            os.system(aEjecutar)
+            print "Ha terminado\r\n"
             #data = my_socket.recv(1024)
             #print 'Recibido: ', data
         elif metodo == 'BYE':
